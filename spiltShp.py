ab8cf5b87edfc7512055bfb63156cfae7970723c
Ermias <ermiasbet@hotmail.com>
2012-12-22 15:13:48 +0100

    Extracts features from shapefiles based on attribute queries
    This code extracts features from shapefiles based on their attribute
    query and saves the output features in a file geodatabase.
    An SQL expression is used within the python function to select a subset
    of features, therefore the process can be slow on larger datasets.

---
+++ spiltShp.py
@@ -0,0 +1,45 @@
+'''
+Created on 17. des. 2012
+
+@author: ermias.TESSINFJELLET
+'''
+import arcpy
+from arcpy import env
+
+env.workspace = r"C:\Data\Projects\u37"
+env.overwriteOutput = "True"
+
+# Create a pollution file geodatabase if it doesn't exist
+try:
+    arcpy.CreateFileGDB_management("C:/Data/Projects/u37", "pollution.gdb")
+except:
+    pass
+
+outWorkspace = "C:/Data/Projects/u37/pollution.gdb/"
+
+fcs = arcpy.ListFeatureClasses()
+
+arcpy.AddMessage(" Executing: subset layer by attribute ...")
+# Put in error trapping in case an error occurs when running tool
+try:
+
+   for fc in fcs:
+    rows = arcpy.SearchCursor(fc)
+    fileN = str.split(str(fc), '.')[0]
+    for row in rows:
+        species = row.Species
+        tissue = row.Tissue
+        year = row.Year
+        expressionS = "\"Species\" = '%s'" %species
+        expressionY = '"Year" = %d' %year
+        expressionT = "\"Tissue\" = '%s'" %tissue
+        arcpy.MakeFeatureLayer_management(fc, "fc_Lyr", expressionS)
+        arcpy.SelectLayerByAttribute_management("fc_Lyr", "NEW_SELECTION", expressionY)
+        arcpy.SelectLayerByAttribute_management("fc_Lyr", "NEW_SELECTION", expressionT)
+        speciesN = ''.join(e for e in species if e.isalnum())
+        yearN = str (int(year))
+        tissueN = str(tissue)
+        arcpy.CopyFeatures_management("fc_Lyr", outWorkspace + fileN + "_" + speciesN + "_" + tissueN + "_" + yearN)
+
+except:
+   print arcpy.GetMessages()
