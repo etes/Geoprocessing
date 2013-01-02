93ccfea53e7f9f4a32cd7468c8175ed52d005202
Ermias <ermiasbet@hotmail.com>
2013-01-02 17:23:19 +0100

    Add field and populate it with ice concentration
    Ice concentration of Ice charts from met.no is catagorized into 6
    classes. These values are textual description in string format. This
    code converts these to numeric values and adds them as new field to each
    shapefile

---
+++ addIceConc.py
@@ -0,0 +1,29 @@
+'''
+Created on 5. sep. 2012
+
+@author: ermias
+'''
+
+import arcpy
+
+arcpy.env.workspace = 'C:/Data/Projects/IsAnalyse/Isdata/barents_shp2'
+
+fcs = arcpy.ListFeatureClasses()
+
+# Add ice concentration field
+# Create Ice type Dictionary
+# Process Ice type in dictionary
+for fc in fcs:
+    arcpy.AddField_management(fc,'IceConc',"SHORT")
+    arcpy.CalculateField_management(fc, "IceConc", "iceConc(!ICE_TYPE!)", "PYTHON",
+                                    "def iceConc(iceType):\
+                                    \n    iceDict = {\
+                                    'Fast Ice':100,\
+                                    'Very Close Drift Ice':95,\
+                                    'Close Drift Ice':80,\
+                                    'Open Drift Ice':55,\
+                                    'Very Open Drift Ice':25,\
+                                    'Open Water':5}\
+                                    \n    return iceDict.get(iceType)")
+
+print "Ice Concentration added successfully."
