de5650e1c5df3e5c544084b79df57452cd6cbde0
Ermias <ermias@npolar.no>
2013-01-08 23:30:54 +0100

    R scripts & data

---
+++ heatmap.R
@@ -0,0 +1,196 @@
+#install.packages("RSQLite")
+#install.packages("XML")
+#install.packages("ggplot2")
+#install.packages(c("maps","mapproj"))
+#install.packages("stalkR_0.02.zip", repos=NULL, type="source")
+#install.packages("C:/Users/ermias/Documents/stalkR.zip", repos=NULL, type="source")
+#install.packages("C:/Users/ermias/Documents/stalkR_0.02.zip",lib="C:/Users/ermias/Documents/R/win-library/2.13", repos=NULL, type="source")
+#install.packages('RgoogleMaps')
+
+
+library(RgoogleMaps)
+Df<-read.csv("pmap3.csv",sep=";",header=FALSE)
+names(Df)<-c("Latitude", "Longitude","key")
+
+bb <- qbbox(lat=range(Df$Latitude), lon=range(Df$Longitude))
+m <- c(mean(Df$Latitude), mean(Df$Longitude))
+zoom <- min(MaxZoom(latrange=bb$latR,lonrange=bb$lonR))
+Map <- GetMap.bbox(bb$lonR, bb$latR, zoom=zoom, maptype="mobile",
+NEWMAP=TRUE, destfile="tempmap.jpg", RETURNIMAGE=TRUE, GRAYSCALE=TRUE)
+tmp <- PlotOnStaticMap(lat=Df$Latitude, lon=Df$Longitude,
+cex=.7,pch=20,col="red", MyMap=Map, NEWMAP=FALSE)
+
+pm=read.csv("pm10.txt",sep=",",header=TRUE)
+pm=pm[,c(1,2,5)]
+bb <- qbbox(lat=range(pm$Lat), lon=range(pm$Long))
+m <- c(mean(pm$Lat), mean(pm$Long))
+zoom <- min(MaxZoom(latrange=bb$latR,lonrange=bb$lonR))
+Map <- GetMap.bbox(bb$lonR, bb$latR, zoom=zoom, maptype="mobile",
+NEWMAP=TRUE, destfile="tempmap.jpg", RETURNIMAGE=TRUE, GRAYSCALE=TRUE)
+tmp <- PlotOnStaticMap(lat=pm$Lat, lon=pm$Long,
+cex=1,pch=20,col="red", MyMap=Map, NEWMAP=FALSE)
+##OR
+tmp <- PlotOnStaticMap(lat=pm$Lat, lon=pm$Long,
+cex=1,pch=20,col=heat.colors(nrow(pm), alpha = pm$pm10), MyMap=Map, NEWMAP=FALSE)
+
+### meusezinc data
+library(rgdal)
+Df=read.csv("meusezinc.csv",sep=" ",header=TRUE)
+coordinates(Df)=~ x + y
+proj4string(Df) = CRS("+init=epsg:28992")
+bbox(Df)
+Df1 = spTransform(Df, CRS("+init=epsg:4326"))
+bbox(Df1)
+Df2=as.data.frame(Df1)
+names(Df2)<-c("zinc","Longitude","Latitude")
+bb <- qbbox(lat=range(Df2$Latitude), lon=range(Df2$Longitude))
+m <- c(mean(Df2$Latitude), mean(Df2$Longitude))
+zoom <- min(MaxZoom(latrange=bb$latR,lonrange=bb$lonR))
+Map <- GetMap.bbox(bb$lonR, bb$latR, zoom=zoom, maptype="mobile",
+NEWMAP=TRUE, destfile="tempmap.jpg", RETURNIMAGE=TRUE, GRAYSCALE=TRUE)
+## rescale=(0.5*(Df2$zinc+max(Df2$zinc))-min(Df2$zinc))/(max(Df2$zinc)-min(Df2$zinc))
+rescaled=(Df2$zinc-min(Df2$zinc))/(max(Df2$zinc)-min(Df2$zinc))
+tmp <- PlotOnStaticMap(lat=Df2$Latitude, lon=Df2$Longitude,
+cex=2,pch=20,col=heat.colors(nrow(Df2), alpha = rescaled), MyMap=Map, NEWMAP=FALSE)
+
+#### Meuse data coordinate transformation
+library(gstat)
+library(rgdal) 
+data(meuse) 
+coordinates(meuse) =~ x + y 
+proj4string(meuse) = CRS("+init=epsg:28992") 
+bbox(meuse) 
+meuse1 = spTransform(meuse, CRS(paste("+proj=stere +lat_0=90", 
+   "+lon_0=0.0 +lat_ts=60.0 +a=6378388 +b=6356912 +x_0=0 +y_0=0"))) 
+bbox(meuse1) 
+meuse2 <- spTransform(meuse1, CRS("+init=epsg:28992")) 
+bbox(meuse2) 
+all.equal(bbox(meuse), bbox(meuse2)) 
+
+###########################################3
+#### Meuse data plot on google
+library(gstat)
+library(rgdal)
+library(ReadImages)
+library(RgoogleMaps)
+data(meuse) 
+coordinates(meuse) =~ x + y 
+proj4string(meuse) = CRS("+init=epsg:28992") 
+bbox(meuse) 
+meuse1 = spTransform(meuse, CRS("+init=epsg:4326")) 
+bbox(meuse1)
+meuse2=as.data.frame(meuse1)
+mzn=meuse2[,c(14,13,4)]
+names(mzn)<-c("Latitude","Longitude","zinc")
+bb <- qbbox(lat=range(mzn$Latitude), lon=range(mzn$Longitude))
+m <- c(mean(mzn$Latitude), mean(mzn$Longitude))
+zoom <- min(MaxZoom(latrange=bb$latR,lonrange=bb$lonR))
+Map <- GetMap.bbox(bb$lonR, bb$latR, zoom=zoom, maptype="mobile",
+NEWMAP=TRUE, destfile="tempmap.jpg", RETURNIMAGE=TRUE, GRAYSCALE=TRUE)
+## rescale=(0.5*(mzn$zinc+max(mzn$zinc))-min(mzn$zinc))/(max(mzn$zinc)-min(mzn$zinc))
+rescaled=(mzn$zinc-min(mzn$zinc))/(max(mzn$zinc)-min(mzn$zinc))
+tmp <- PlotOnStaticMap(lat=mzn$Latitude, lon=mzn$Longitude,
+cex=2,pch=20,col=heat.colors(nrow(mzn), alpha = rescaled), MyMap=Map, NEWMAP=FALSE)
+
+
+
+##### OPEN STREET MAP
+library(RgoogleMaps)
+png(filename="GetMap.OSM_%03d_med.png", width=480, height=480)
+CologneMap <- GetMap.OSM(lonR= c(6.89, 7.09), latR = c(50.87, 51), scale = 150000, destfile = "Cologne.png");
+  	PlotOnStaticMap(CologneMap, mar=rep(4,4), NEWMAP = FALSE, TrueProj = FALSE, axes= TRUE);
+		
+		PrincetonMap <- GetMap.OSM(lonR= c(-74.67102, -74.63943), latR = c(40.33804,40.3556), scale = 12500, destfile = "Princeton.png");
+		png("PrincetonWithAxes.png", 1004, 732)
+        PlotOnStaticMap(PrincetonMap, axes = TRUE, mar = rep(4,4));
+        dev.off()
+        
+###### Meuse on OSM
+
+library(gstat)
+library(rgdal)
+library(ReadImages)
+library(RgoogleMaps)
+data(meuse) 
+coordinates(meuse) =~ x + y 
+proj4string(meuse) = CRS("+init=epsg:28992") 
+bbox(meuse) 
+meuse1 = spTransform(meuse, CRS("+init=epsg:4326")) 
+bbox(meuse1)
+meuse2=as.data.frame(meuse1)
+mzn=meuse2[,c(14,13,4)]
+names(mzn)<-c("Latitude","Longitude","zinc")
+bb <- qbbox(lat=range(mzn$Latitude), lon=range(mzn$Longitude))
+m <- c(mean(mzn$Latitude), mean(mzn$Longitude))
+zoom <- min(MaxZoom(latrange=bb$latR,lonrange=bb$lonR))
+Map <- GetMap.bbox(bb$lonR, bb$latR, zoom=zoom, maptype="mobile", ## or satellite
+NEWMAP=TRUE, destfile="tempmap.jpg", RETURNIMAGE=TRUE, GRAYSCALE=FALSE)
+##rescaled=(0.5*(mzn$zinc+max(mzn$zinc))-min(mzn$zinc))/(max(mzn$zinc)-min(mzn$zinc))
+rescaled=(mzn$zinc-min(mzn$zinc))/(max(mzn$zinc)-min(mzn$zinc))
+tmp <- PlotOnStaticMap(lat=mzn$Latitude, lon=mzn$Longitude,
+cex=2,pch=20,col=heat.colors(nrow(mzn), alpha = rescaled), MyMap=Map, NEWMAP=TRUE)
+
+### with OSM
+MMap <- GetMap.OSM(bb$lonR, bb$latR, scale = 12500, destfile = "tempmap.png", NEWMAP = TRUE, RETURNIMAGE=TRUE);
+tmp <- PlotOnStaticMap(lat=mzn$Latitude, lon=mzn$Longitude,
+cex=2,pch=20,col=heat.colors(nrow(mzn), alpha = rescaled),axes = TRUE, MyMap=MMap, mar = rep(4,4), NEWMAP=FALSE)
+
+
+####### Coso major faults
+#install.packages("geomapdata")
+library(rgdal)
+library(ReadImages)
+library(RgoogleMaps)
+library(geomapdata)
+data(cosomap)
+bb <- qbbox(lon=cosomap$POINTS$lon-360,lat=cosomap$POINTS$lat)
+MyMap <- GetMap.bbox(bb$lonR, bb$latR,destfile = "Coso.png",
+maptype="satellite",zoom=11)
+tmp <- PlotOnStaticMap(MyMap,lon=cosomap$POINTS$lon-360,
+lat=cosomap$POINTS$lat, pch=20,cex = .5,col='red',verbose=0);
+
+dev.off()
+############ earthquake data
+library(rgdal)
+library(ReadImages)
+library(RgoogleMaps)
+#setwd("F:././Documents/R/")
+pm<-read.csv("eqs7day-M1.txt", header=TRUE)
+bb <- qbbox(lat=range(pm$Lat), lon=range(pm$Lon))
+m <- c(mean(pm$Lat), mean(pm$Lon))
+zoom <- min(MaxZoom(latrange=bb$latR,lonrange=bb$lonR))
+Map <- GetMap.bbox(bb$lonR, bb$latR, zoom=zoom, maptype="mobile",
+NEWMAP=TRUE, destfile="tempmap.jpg", RETURNIMAGE=TRUE, GRAYSCALE=TRUE)
+tmp <- PlotOnStaticMap(lat=pm$Lat, lon=pm$Long,
+cex=1,pch=20,col="red", MyMap=Map, NEWMAP=FALSE)
+##OR
+
+tmp <- PlotOnStaticMap(lat=pm$Lat, lon=pm$Long,
+cex=1,pch=20,col=heat.colors(nrow(pm), alpha = pm$Magnitude), MyMap=Map, NEWMAP=FALSE)
+
+######## Geological Data
+
+library(rgdal)
+library(ReadImages)
+library(RgoogleMaps)
+setwd("F:././Documents/R/")
+geoch<-read.csv("020093942011521183834.csv", header=TRUE)
+geo<-geoch[,c(6,7,8,9)]
+names(geo)<-c("Lat","Lon","LatMax","LonMax")
+geo<-geo[-(99:109),]
+pm<-geo
+bb <- qbbox(lat=range(pm$Lat), lon=range(pm$Lon))
+m <- c(mean(pm$Lat), mean(pm$Lon))
+zoom <- min(MaxZoom(latrange=bb$latR,lonrange=bb$lonR))
+Map <- GetMap.bbox(bb$lonR, bb$latR, zoom=zoom, maptype="mobile",
+NEWMAP=TRUE, destfile="tempmap.jpg", RETURNIMAGE=TRUE, GRAYSCALE=TRUE)
+tmp <- PlotOnStaticMap(lat=pm$Lat, lon=pm$Long,
+cex=1,pch=20,col="red", MyMap=Map, NEWMAP=FALSE)
+##OR
+
+tmp <- PlotOnStaticMap(lat=pm$Lat, lon=pm$Long,
+cex=1,pch=20,col=heat.colors(nrow(pm), alpha = pm$Magnitude), MyMap=Map, NEWMAP=FALSE)
+
+
+
+
---
+++ mapprojection.R
@@ -0,0 +1,190 @@
+#interpolation of xyz data and projection onto a map
+map.xyz <- function(xyz_data, file_name="map.xyz.png",
+ 
+projection="stereographic", orientation=NULL,
+ 
+par=NULL, fill=FALSE, nside=40, breaks=100, res=100,
+ 
+xlim = NULL, ylim = NULL
+ 
+){
+ 
+ 
+ if(length(which(rowSums(is.na(xyz_data)) != 0)) > 0){
+ 
+  xyz_data <- xyz_data[-which(rowSums(is.na(xyz_data)) != 0),]
+ 
+ }
+ 
+ 
+ require(akima)
+ 
+ require(maps)
+ 
+ require(mapproj)
+ 
+ 
+ 
+ temp<-interp(
+ 
+  xyz_data[,1], xyz_data[,2], xyz_data[,3],  
+ 
+  xo=seq(min(xyz_data[,1]), max(xyz_data[,1]), length = nside), 
+ 
+  yo=seq(min(xyz_data[,2]), max(xyz_data[,2]), length = nside, extrap=TRUE)
+ 
+ )
+ 
+ 
+ 
+ polys<-vector("list", length(as.vector(temp$z)))
+ 
+ for(i in 1:length(polys)){
+ 
+  lonx <- pos2coord(pos=i, mat=temp$z)$coord[1]
+ 
+  laty <- pos2coord(pos=i, mat=temp$z)$coord[2]
+ 
+  ifelse(laty < length(temp$y), neigh_y<-c(laty+1,lonx), neigh_y<-c(laty-1,lonx))
+ 
+  ifelse(lonx < length(temp$x), neigh_x<-c(laty,lonx+1), neigh_x<-c(laty,lonx-1)) 
+ 
+  dist_y <- earth.dist(temp$x[lonx], temp$y[laty], temp$x[neigh_y[2]], temp$y[neigh_y[1]])
+ 
+  dist_x <- earth.dist(temp$x[lonx], temp$y[laty], temp$x[neigh_x[2]], temp$y[neigh_x[1]])
+ 
+  s1 = new.lon.lat(temp$x[lonx], temp$y[laty], 180, dist_y/2)
+ 
+  s3 = new.lon.lat(temp$x[lonx], temp$y[laty], 0, dist_y/2)
+ 
+  s2 = new.lon.lat(temp$x[lonx], temp$y[laty], 270, dist_x/2)
+ 
+  s4 = new.lon.lat(temp$x[lonx], temp$y[laty], 90, dist_x/2)
+ 
+  polys[[i]] = cbind(c(s2[1], s2[1], s4[1], s4[1]), c(s1[2], s3[2], s3[2], s1[2]))
+ 
+ }
+ 
+ 
+ 
+ M.range=range(temp$z, na.rm=TRUE)
+ 
+ M.breaks <- pretty(M.range, n=breaks)
+ 
+ M.cols=colorRampPalette(c("blue","cyan", "yellow", "red"),space="rgb")
+ 
+ colorlut <- M.cols(length(M.breaks)) # color lookup table
+ 
+ colorvalues <- colorlut[((as.vector(temp$z)-M.breaks[1])/(range(M.breaks)[2]-range(M.breaks)[1])*length(M.breaks))+1] # assign colors to heights for each point
+ 
+ if(is.null(xlim)){xlim <- range(xyz_data[,1], na.rm=TRUE)}
+ 
+ if(is.null(ylim)){ylim <- range(xyz_data[,2], na.rm=TRUE)}
+ 
+ png(filename=file_name, res=res)
+ 
+ map("world",projection=projection, orientation=orientation, par=par, fill=fill, xlim=xlim, ylim=ylim)
+ 
+ for(i in 1:length(polys)){
+ 
+  polygon(mapproject(x=polys[[i]][,1], y=polys[[i]][,2]), col=colorvalues[i], border=NA)
+ 
+  print(i)
+ 
+ }
+ 
+ map("world", add=TRUE, projection="", par=par, fill=fill, xlim=xlim, ylim=ylim)
+ 
+ map.grid(c(-180, 180, -80, 80), nx=10, ny=18, labels=FALSE, col="grey")
+ 
+ dev.off()
+ 
+}
+
+pos2coord<-function(pos=NULL, coord=NULL, mat=NULL){
+ 
+ if(is.null(pos) & is.null(coord) & is.null(mat)){
+ 
+  stop("must supply either 'pos' or 'coord', and 'mat'")
+ 
+ }
+ 
+ if(is.null(pos) & !is.null(coord) & !is.null(mat)){
+ 
+  pos <- ((coord[2]-1)*dim(mat)[1])+coord[1] 
+ 
+ }
+ 
+ if(!is.null(pos) & is.null(coord) & !is.null(mat)){
+ 
+  coord <- NA*c(1:2)
+ 
+  coord[1] <- ((pos-1) %% dim(mat)[1]) +1
+ 
+  coord[2] <- ((pos-1) %/% dim(mat)[1])+1
+ 
+ }
+ 
+ list(pos=pos, coord=coord)
+ 
+}
+
+#distance in kilometers between two long/lat positions (from "fossil" package)
+earth.dist <- function (long1, lat1, long2, lat2) 
+{
+    rad <- pi/180
+    a1 <- lat1 * rad
+    a2 <- long1 * rad
+    b1 <- lat2 * rad
+    b2 <- long2 * rad
+    dlon <- b2 - a2
+    dlat <- b1 - a1
+    a <- (sin(dlat/2))^2 + cos(a1) * cos(b1) * (sin(dlon/2))^2
+    c <- 2 * atan2(sqrt(a), sqrt(1 - a))
+    R <- 6378.145
+    d <- R * c
+    return(d)
+}
+
+#new long/lat position given a starting lon/lat and degree bearing (from "fossil" package)
+new.lon.lat <- function (lon, lat, bearing, distance) {
+    rad <- pi/180
+    a1 <- lat * rad
+    a2 <- lon * rad
+    tc <- bearing * rad
+    d <- distance/6378.145
+    nlat <- asin(sin(a1) * cos(d) + cos(a1) * sin(d) * cos(tc))
+    dlon <- atan2(sin(tc) * sin(d) * cos(a1), cos(d) - sin(a1) * 
+        sin(nlat))
+    nlon <- ((a2 + dlon + pi)%%(2 * pi)) - pi
+    npts <- c(nlon/rad, nlat/rad)
+    return(npts)
+}
+
+#degree bearing between two long/lat positions (from "fossil" package)
+earth.bear <- function (long1, lat1, long2, lat2) 
+{
+    rad <- pi/180
+    a1 <- lat1 * rad
+    a2 <- long1 * rad
+    b1 <- lat2 * rad
+    b2 <- long2 * rad
+    dlon <- b2 - a2
+    bear <- atan2(sin(dlon) * cos(b1), cos(a1) * sin(b1) - sin(a1) * 
+        cos(b1) * cos(dlon))
+    deg <- (bear%%(2 * pi)) * (180/pi)
+    return(deg)
+}
+
+lon.lat.filter <- function (lon_vector, lat_vector, west, east, north, south) 
+{
+ if(west>east) {
+  lon_vector_new=replace(lon_vector, which(lon_vector<0), lon_vector[which(lon_vector<0)]+360)
+  east_new=east+360
+ } else {
+  lon_vector_new=lon_vector
+  east_new=east
+ }
+  hits=which(lon_vector_new < east_new & lon_vector_new > west & lat_vector < north & lat_vector > south)
+ return(hits)
+} 
