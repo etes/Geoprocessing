f75e63a2dbff573ca92843d4fa1a258666a85ce7
etes <ermias@npolar.no>
2013-07-02 09:52:44 +0200

    plot borehole locations on Google Maps with RgoogleMaps

---
+++ boreholes.R
@@ -0,0 +1,33 @@
+library(rgdal)
+library(RgoogleMaps)
+boreholes<- read.csv("boreholes.csv", sep=",", header=TRUE)
+bh_grd <- SpatialPoints(boreholes[,c("East","North")],
+                        proj4string=CRS("+proj=utm +zone=32 +datum=WGS84"))
+bholes<- SpatialPointsDataFrame(bh_grd,boreholes[,-c(2,3)])
+bholes<-bholes[,-c(2,3)]
+bh_geo<- spTransform(bholes, CRS("+proj=longlat +datum=WGS84"))
+
+North<-coordinates(bh_geo)[,2]
+East<-coordinates(bh_geo)[,1]
+bb <- qbbox(lat=range(North), lon=range(East))
+m <- c(mean(North), mean(North))
+zoom <- min(MaxZoom(latrange=bb$latR,lonrange=bb$lonR))
+Map <- GetMap.bbox(bb$lonR, bb$latR, zoom=zoom, maptype="mobile",
+                   NEWMAP=F, destfile="map.png", RETURNIMAGE=T);
+tmp <- PlotOnStaticMap(Map,lat=North, lon=East,
+                       cex=1.5,pch=20,col="red", add=F, verbose=0);
+tmp <- TextOnStaticMap(Map,lat=North, lon=East+.0007, cex=.8, font=4,
+                       col="black", labels= boreholes$Number, add=T, verbose=0);
+tmp <- PlotArrowsOnStaticMap(Map, lat0=min(North),lon0=max(East)+.006,
+                             lat1=min(North)+.0006,lon1=max(East)+.006,lwd=6,col="black",add=T);
+
+
+#Satellite image as a base map
+SMap <- GetMap.bbox(bb$lonR, bb$latR, zoom=zoom, maptype="satellite",
+                   NEWMAP=F, destfile="map.png", RETURNIMAGE=TRUE);
+tmp <- PlotOnStaticMap(SMap,lat=North, lon=East,
+                       cex=1.5,pch=20,col="red", add=F, verbose=0);
+tmp <- TextOnStaticMap(SMap,lat=North, lon=East+.0007, cex=.8, font=4,
+                       col="yellow", labels= boreholes$Number, add=T, verbose=0);
+tmp <- PlotArrowsOnStaticMap(SMap, lat0=min(North),lon0=max(East)+.006,
+                             lat1=min(North)+.0006,lon1=max(East)+.006,lwd=6,col="yellow",add=T);
