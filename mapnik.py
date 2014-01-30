d98dd772215dd6176f8b59d673def0277bb27ef7
etes <ermias@npolar.no>
2014-01-30 23:01:40 +0100

    update data source

--- mapnik.py
+++ mapnik.py
@@ -1,16 +1,17 @@
+# uses mapnik 2
 import mapnik2
 symbolizer = mapnik2.PolygonSymbolizer(
-mapnik2.Color("darkgreen"))
+mapnik2.Color("darkgrey"))
 rule = mapnik2.Rule()
 rule.symbols.append(symbolizer)
 style = mapnik2.Style()
 style.rules.append(rule)
 layer = mapnik2.Layer("mapLayer")
 layer.datasource = mapnik2.Shapefile(
-file="..//TM_WORLD_BORDERS-0.3/TM_WORLD_BORDERS-0.3.shp")
+file="/data/82945364-10m-admin-0-countries.shp")
 layer.styles.append("mapStyle")
 map = mapnik2.Map(2400, 1200)
-map.background = mapnik2.Color("steelblue")
+map.background = mapnik2.Color("black")
 map.append_style("mapStyle", style)
 map.layers.append(layer)
 map.zoom_all()
